01cc2f84254a65e9408f45678c672256751608d7
Oliver Cook <jocook@users.noreply.github.com>
2017-06-02 08:56:20 -0400

    Added Files

---
+++ ucsc_create_pools.py
@@ -0,0 +1,41 @@
+import sys
+import json
+import csv
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) < 2:
+            print "Usage: %s <JSON settings file>" % sys.argv[0]
+            print "       <settings file>: access settings (IP/user/password)"
+            sys.exit(0)
+        f = open(sys.argv[1], 'r')
+        settings_file = json.load(f)
+
+        from ucscsdk.ucschandle import UcscHandle
+        handle = UcscHandle(ip=settings_file['ip'], username=settings_file['user'], password=settings_file['pw'])
+        handle.login()
+        
+        print "Creating global Pools and Policies:" #% (template)
+
+        from ucscsdk.mometa.fabric.FabricVlan import FabricVlan
+
+        mo = FabricVlan(parent_mo_or_dn="domaingroup-root/fabric/lan",
+                        sharing="none",
+                        name="192-10.10.192.0_24",
+                        id="192",
+                        policy_owner="local",
+                        default_net="no",
+                        )
+
+
+        handle.add_mo(mo, True)
+        handle.commit()
+
+        handle.logout()
+
+    except Exception, err:
+        print "Exception:", str(err)
+        import traceback, sys
+        print '-'*60
+        traceback.print_exc(file=sys.stdout)
+        print '-'*60
---
+++ ucsc_destroy_UuidPool.py
@@ -0,0 +1,33 @@
+import sys
+import json
+import csv
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) < 2:
+            print "Usage: %s <JSON settings file>" % sys.argv[0]
+            print "       <settings file>: access settings (IP/user/password)"
+            sys.exit(0)
+        f = open(sys.argv[1], 'r')
+        settings_file = json.load(f)
+
+        from ucscsdk.ucschandle import UcscHandle
+        handle = UcscHandle(ip=settings_file['ip'], username=settings_file['user'], password=settings_file['pw'])
+        handle.login()
+        
+        print "Deleting UUID Pool:" #% (template)
+
+        mo = handle.query_dn("org-root/uuid-pool-G-UUID-Pool")
+        handle.remove_mo(mo)
+
+        handle.commit()
+
+
+        handle.logout()
+
+    except Exception, err:
+        print "Exception:", str(err)
+        import traceback, sys
+        print '-'*60
+        traceback.print_exc(file=sys.stdout)
+        print '-'*60
