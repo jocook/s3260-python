8a837f1df97c4fc8d40487f6ba98f432d34d9a2d
Oliver Cook <jocook@users.noreply.github.com>
2017-06-08 14:48:45 -0400

    added UCSC Script to generate vMedia Policy

---
+++ ucsc_create_vMediaPolicy.py
@@ -0,0 +1,41 @@
+import sys
+import json
+import csv
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) < 2:
+            print "Usage: %s <JSON settings file>" % sys.argv[0]
+            print "       <settings file>: access settings (IP/user/password)"
+            sys.exit(0)
+        f = open(sys.argv[1], 'r')
+        settings_file = json.load(f)
+
+        from ucscsdk.ucschandle import UcscHandle
+        handle = UcscHandle(ip=settings_file['ip'], username=settings_file['user'], password=settings_file['pw'])
+        handle.login()
+        
+        print "Creating vMedia Policy in UCS Central:" #% (template)
+
+        from ucscsdk.mometa.cimcvmedia.CimcvmediaMountConfigPolicy import CimcvmediaMountConfigPolicy
+        from ucscsdk.mometa.cimcvmedia.CimcvmediaConfigMountEntry import CimcvmediaConfigMountEntry
+
+        mo = CimcvmediaMountConfigPolicy(parent_mo_or_dn="org-root", retry_on_mount_fail="yes", name="RHEL-7-3", descr="RHEL 7.3")
+        mo_1 = CimcvmediaConfigMountEntry(parent_mo_or_dn=mo, user_id="", description="RHEL 7.3", remote_ip_address="10.156.10.10", remote_port="0", image_name_variable="none", auth_option="default", mapping_name="RHEL-7-3", image_file_name="RHEL7.3_jabtl1203.iso", device_type="cdd", mount_protocol="nfs", password="", image_path="/export/media/")
+        handle.add_mo(mo)
+
+        handle.commit()
+
+
+
+
+
+
+        handle.logout()
+
+    except Exception, err:
+        print "Exception:", str(err)
+        import traceback, sys
+        print '-'*60
+        traceback.print_exc(file=sys.stdout)
+        print '-'*60
